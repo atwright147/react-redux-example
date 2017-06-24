66309cd8c1a3295ca6a9bb7622c54aed0cbe93d0
Andy Wright <atwright147@gmail.com>
2017-06-24 19:29:50 +0100

    add components as prescribed by Cory Houser

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { reducer as formReducer } from 'redux-form'
-import { createStore, combineReducers } from 'redux'
-// import Main from './app/components/Main';
-import Input from './app/components/Input';
 
-const rootReducer = combineReducers({
-    // ...your other reducers here
-    // you have to pass formReducer under 'form' key,
-    // for custom keys look up the docs for 'getFormState'
-    form: formReducer
-})
+import configureStore from './app/store/configureStore';
+import CoursesPage from './app/components/Course/CoursesPage';
 
-const store = createStore(
-    rootReducer, /* preloadedState, */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//     rootReducer, /* preloadedState, */
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
+const store = configureStore();
 
 render (
     <Provider store={store}>
-        <Input />
-    </Provider>,
-    document.getElementById('app')
+        <CoursesPage />
+    </Provider>
+    ,document.getElementById('app')
 );
